86f65eb464c2c783f2781c99f854c37019760b59
B3rg4r4 <pedrobergara5@gmail.com>
2024-05-16 15:39:48 -0300

    Alteração de ifs e elses para match case, organização de funções, criação de listas

--- Sprint1 python.py
+++ Sprint1 python.py
@@ -1,43 +1,90 @@
-print("Bem vindo ao Porta Car-Fix")
+
 #print para receber o usuario no programa
+print("Bem vindo ao Porta Car-Fix")
 
-intecao = str(input("O que deseja fazer ? \n Você pode acessar seu perfil digitando '1' \n Você pode ver as opções de planos disponiveis e quais você  tem acesso digitando '2' \n Alguma duvida digite '3'\n Caso queira dar um feedback digite '4' \n "))
-#intação para saber qual a inteção do usuario dentro do menu
-while intecao != "1" and intecao != "2" and intecao != "3" and intecao != "4":
-#laço de repetiçao para caso o usuario insira uma opção não cadastrada
-    intecao = int(input("Aqui temos algumas opções possiveis dentro do nosso site \n Você pode acessar seu perfil digitando '1' \n Você pode ver as opções de planos disponiveis e quais você  tem acesso digitando '2'  \n Alguma duvida digite '3'\n Caso queira dar um feedback digite '4' \n "))
+#Criação de listas para armazenar e organizar dados inseridos pelo usuario
+emails = []
+nomes = []
+placas = []
+feedbacks = []
+duvidas = []
 
-if intecao == "1":
-    #condição caso o usuario escolha a opção 1 do menu (acessar perfil)
-    cadastro = str(input("ja tem uma conta ? \n"))
-    while cadastro != "sim" and cadastro != "não":
-        #laço caso usuario digite alguma coisa errada ou uma opção que não esteja cadaastrada 
+#função para caso o usuario escolha fazer cadastro no programa
+def cadastro():
+    while True:
         cadastro = str(input("ja tem uma conta ? \n"))
-    if cadastro == "sim":
-        email = str(input("Digite seu email: "))
-        senha = str(input("Digite sua senha: "))
-    else:
-        print("Crie sua conta aqui")
-        email = str(input("Digite seu email: "))
-        senha = str(input("Digite sua senha: "))
-    print(f"O email cadastrado foi {email}")
+        #laço caso o usuario escolha uma função que não esta presente nas opções
+        while cadastro != "sim" and cadastro != "não":
+            cadastro = str(input("ja tem uma conta ? \n"))
+        if cadastro == "sim":
+            email = str(input("Digite seu email: "))
+            senha = str(input("Digite sua senha: "))
+        else:
+
+            print("Crie sua conta aqui")
+
+            nomes.append(str(input("Digite seu nome:\n")))
+            emails.append(str(input("Digite seu email:\n")))
+            senha = str(input("Digite sua senha:\n"))
+            confirmSenha = str(input("Confirme sua senha:\n"))
+            while senha != confirmSenha:
+                print("ERRO!Algum campo foi preenchido errado ")
+                senha = str(input("Digite sua senha:\n"))
+                confirmSenha = (str(input("Confirme sua senha:\n")))
+            print("Seu cadastro foi realizado com sucesso!")
+            cadastrarVeiculo = str(input("Deseja cadastrar algum veiculo ?"))
+            match cadastrarVeiculo:
+                case "sim":
+                    placas.append(str(input("Para Começarmos, digite os digitos da sua placa:\n")))
+                case "não":
+                    print("Tudo bem !! Obrigado por utilizar o Porta Car-Fix, volte sempre que precisar de uma ajuda com seus veiculos ")
+                case _:
+                    print("Opção não cadastrada")
+
+
+            print(f"O email cadastrado foi {emails}")
+
+##função para alterar alguma informação cadastrada
+def mudarInfor ():
+    print("Escolha a informação que deseja alterar:")
+
+#função caso o usuario escolha ver os planos do nosso serviço
+def planos():
+    while True:
+        planos = str(input("Quer visualizar todos nossos planos ? \n"))
+        #laço caso o usuario escolha uma opção que não esta disponivel
+        while planos != "sim" and planos != "não":
+            planos = str(input("Quer visualizar todos nossos planos ? \n"))
+        if planos == "sim":
+            print("Temos o planos de: Cobertura Total, que concede acesso a todos serviços dentro do nosso site \nCobertura Parcial, que vai de acordo com a sua região, automovel, e equipamento\nCobertura Mecânica, nesse plano tera acesso apenas acesso a analise de um mecanico ")
+        else:
+            print("para vizualiar seus planos faça cadastro")
+
+#função caso o usuario escolha mandar uma duvida para a central de atendimento
+def duvida():
+    while True:
+        duvidas = str(input("digite sua duvida, referente ao auto atendimento, a resposta pode ser rapida ou levar horas: "))
+
+#função caso o usuario queira dar um feedback para a empresa
+def feedback():
+    while True:
+        feedbacks = str(input("Digite aqui seu feedback para que podemos melhorar nosso serviço e oferecer uma melhor entrega de resultados: "))
+
+while True:
+
+    intencao = str(input("O que deseja fazer ? \n Você pode acessar seu perfil digitando '1' \n Você pode ver as opções de planos disponiveis e quais você tem acesso digitando '2' \n Alguma duvida digite '3'\n Caso queira dar um feedback digite '4' \n "))
     
-elif intecao == "2":
-    #condição caso o usuario escolha a segunda opção do menu(Vizualisar planos)
-    planos = str(input("Quer visualizar todos nossos planos ? \n"))
-    while planos != "sim" and planos != "não":
-        #laço caso usuario digite alguma coisa errada ou uma opção que não esteja cadaastrada 
-         planos = str(input("Quer visualizar todos nossos planos ? \n"))
-    if planos == "sim":
-        print("Temos o planos de: Cobertura Total, que concede acesso a todos serviços dentro do nosso site \nCobertura Parcial, que vai de acordo com a sua região, automovel, e equipamento\nCobertura Mecânica, nesse plano tera acesso apenas acesso a analise de um mecanico ")
-    else:
-        print("para vizualiar seus planos faça cadastro")
-elif intecao == 3:
-    #condição caso a pessoa escolha a terceira opção do menu(caixa de duvidas)
-    duvida = str(input("digite sua duvida, referente ao auto atendimento, a resposta pode ser rapida ou levar horas: "))
+    while intencao != "1" and intencao != "2" and intencao != "3" and intencao != "4":
+    #laço de repetiçao para caso o usuario insira uma opção não cadastrada
+        intencao  = str(input("Aqui temos algumas opções possiveis dentro do nosso site\n Você pode acessar seu perfil digitando '1', caso queira mudar alguma informção de cadastro digite 'alterar'\n Você pode ver as opções de planos disponiveis e quais você  tem acesso digitando '2'\n Alguma duvida digite '3'\n Caso queira dar um feedback digite '4' \n "))
+    match intencao:
+        case "1":cadastro()
 
-else:
-    #codição caso o usuario escolha a quarta opção do menu(dar um feedback)
-    feedback = str(input("Digite aqui seu feedback para que possamos melhorar nosso serviço e oferecer uma melhor entrega de resultados: "))
+        case "2":planos()
 
+        case "3":duvida()
 
+        case "4":feedback()
+
+        case _:
+            print("Opção não cadastrada")
